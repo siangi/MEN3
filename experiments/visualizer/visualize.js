07b88688660ae3e18716e981444cf2105d912f46
siangi <siangi@gmx.ch>
2021-11-16 18:38:48 +0100

    it works! although not cleanly

--- experiments/visualizer/visualize.js
+++ experiments/visualizer/visualize.js
@@ -20,37 +20,24 @@ function createVisualizer(){
     updateVisualization();
 
     MotionPathPlugin.convertToPath("#rectPath", true);
-    createLoopForLetters("WOD")
+    createLoopForLetters("star spangled banner")
 }
 
 function createLoopForLetters(text){
     let visSVG = document.querySelector("#visualizer");
     let offset = 1/text.length;
     let simultTimeline = gsap.timeline({repeat: -1});
+    let letters = [];
     
     for (let idx = 0; idx < text.length; idx++){
         let newLetter = `<text x="10" y="10" id="letter${[idx]}" class="visualizerText">${text[idx]}</text>`
         visSVG.innerHTML += newLetter;
-        simultTimeline.to("#letter" + idx, 
-        {
-            duration: 20, 
-            motionPath:
-            {
-                path:"#rectPath",
-                align:"#rectPath",
-                autoRotate:true,
-                alignOrigin: [0.5, 0.5],
-                start: offset*idx
-            }, 
-            ease: "none"   
-
-        }, 0);
-        
+        letters.push(document.querySelector("#letter" + idx))    
     }
-   
-        simultTimeline.to("#letter0", 
+        console.log(letters);
+        gsap.to(".visualizerText", 
         {
-            duration: 20, 
+            duration: 5, 
             motionPath:
             {
                 path:"#rectPath",
@@ -59,23 +46,43 @@ function createLoopForLetters(text){
                 alignOrigin: [0.5, 0.5],
                 start: 0
             }, 
-            ease: "none"   
-
+            stagger:{
+                each: 0.12,
+                repeat: -1
+            },
+            ease: "none",   
+            repeat: -1
         }, 0);
-        simultTimeline.to("#letter1", 
-        {
-            duration: 20, 
-            motionPath:
-            {
-                path:"#rectPath",
-                align:"#rectPath",
-                autoRotate:true,
-                alignOrigin: [0.5, 0.5],
-                start: 0
-            }, 
-            ease: "none"   
 
-        }, 0);
+        // gsap.to("#letter1", 
+        //     {
+        //         duration: 5, 
+        //         motionPath:
+        //         {
+        //             path:"#rectPath",
+        //             align:"#rectPath",
+        //             autoRotate:true,
+        //             alignOrigin: [0.5, 0.5],
+        //             start: 0.1
+        //         }, 
+        //         ease: "none",   
+        //         repeat: -1
+        //     }, 0);
+
+        // gsap.to("letter2", 
+        //     {
+        //         duration: 5, 
+        //         motionPath:
+        //         {
+        //             path:"#rectPath",
+        //             align:"#rectPath",
+        //             autoRotate:true,
+        //             alignOrigin: [0.5, 0.5],
+        //             start: 0.2
+        //         }, 
+        //         ease: "none",   
+        //         repeat: -1
+        //     }, 0);
 }
 
 function updateVisualization(){
@@ -83,7 +90,7 @@ function updateVisualization(){
     let dataArray = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteTimeDomainData(dataArray);
     let scaleFact = scaleTo(dataArray[60], 128, 132, 1, 1.2)  
-    gsap.to("#w", {scale: scaleFact, duration: 0.1}); 
+    gsap.to(".visualizerText", {scale: scaleFact, duration: 0.1}); 
 }
 
 function scaleTo (number, inMin, inMax, outMin, outMax) {
