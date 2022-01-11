9b665d7b9867c9329d62ab5789396f20ce64ff64
siangi <siangi@gmx.ch>
2022-01-11 14:38:30 +0100

    Columns now with a grid, second section

--- full/script/scrollAnimations.js
+++ full/script/scrollAnimations.js
@@ -2,9 +2,9 @@ window.onload = () => {
     gsap.registerPlugin(ScrollTrigger)
     setupDeathSectAnimations();
     pinSectionTitle();
+    starAnimations();
 }
 
-// make this applicable for all
 function setupDeathSectAnimations(){
     gsap.to(".section", {
         backgroundColor: "#000000",
@@ -14,7 +14,37 @@ function setupDeathSectAnimations(){
             end: "top top",
             scrub: true
         },
-    })
+    });
+
+    gsap.to(".section", {
+        backgroundColor: "#EECB00",
+        scrollTrigger: {
+            trigger: "#hendrixExperienceAnimation",
+            start: "top center",
+            end: "top top",
+            scrub: true
+        },
+    });
+
+    gsap.to(".section", {
+        backgroundColor: "#474747",
+        scrollTrigger: {
+            trigger: "#woodstockPerformance",
+            start: "top center",
+            end: "top top",
+            scrub: true
+        },
+    });
+
+    gsap.to(".section", {
+        backgroundColor: "#0F090F",
+        scrollTrigger: {
+            trigger: "#downfall",
+            start: "top center",
+            end: "top top",
+            scrub: true
+        },
+    });
 }
 
 function pinSectionTitle(){
@@ -24,4 +54,42 @@ function pinSectionTitle(){
         start: "top top",
         end: "bottom bottom"
     })
+}
+
+function starAnimations(){
+    starsTimeline = gsap.timeline();
+    starsTimeline.to("#star1, #star2", {transformOrigin: "center center"})
+    starsTimeline.to("#star1", 
+    {
+        rotation: "360",
+        duration: 8,
+        ease: "none",
+        repeat: -1
+    }, 0);
+
+    starsTimeline.to("#star2", 
+    {
+        rotation: "-360",
+        duration: 8,
+        ease: "none",
+        repeat: -1
+    }, 0);
+
+    starsTimeline.to("#star1",
+    {
+        scale: (1.4, 1.4),
+        duration: 2,
+        yoyo: true,
+        repeat: -1
+    }, 0);
+
+    starsTimeline.to("#star2",
+    {
+        scale: (1.0, 1.0),
+        duration: 2,
+        yoyo: true,
+        repeat: -1
+    }, 0)
+    // transform origin stimmt irgendwie noch nicht!!??
+    // starsTimeline.pause();
 }
